def609a5cb657e41513fddc510ec19bbd8a94a71
Michele <michelecarolineferreira@hotmail.com>
2022-02-02 14:25:40 -0300

    Adicionando exercicio de Orientação a Objetos utilizando Conta Bancária, utilizando métodos getters, setters e construtores

---
+++ ExercicioContaBancaria/src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.BankAccount;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter account number:");
+		String numberAccount = sc.nextLine();
+		
+		System.out.println("Enter account holder:");
+		String holderAccount = sc.nextLine();
+		
+		System.out.println("Is there a initial deposit (y/n)?");
+		char yesOrNot = sc.next().charAt(0);
+		
+		double depositValue = 0;
+		
+		if (yesOrNot == 'y') {
+			System.out.print("Enter initial deposit value:");
+			depositValue = sc.nextDouble();
+			}
+	
+		BankAccount bankAccount = (yesOrNot == 'y') ?
+				               new BankAccount(numberAccount, holderAccount, depositValue) 
+				               : new BankAccount(numberAccount, holderAccount);
+		System.out.println(bankAccount);
+		
+		
+		System.out.println("Enter a deposit value:");
+		bankAccount.deposit(sc.nextDouble());
+		System.out.println(bankAccount);
+		
+		System.out.println("Enter a withdraw value:");
+		bankAccount.withdraw(sc.nextDouble());
+		System.out.println(bankAccount);
+		
+		sc.close();
+	}
+
+}
---
+++ ExercicioContaBancaria/src/entities/BankAccount.java
@@ -0,0 +1,51 @@
+package entities;
+
+public class BankAccount {
+	
+	private String numberAccount;
+	private String holderAccount;
+	private double balance;
+	
+	public BankAccount(String numberAccount, String holderAccount, double balance) {
+		this.numberAccount = numberAccount;
+		this.holderAccount = holderAccount;
+		this.balance = balance;
+	}
+
+	public BankAccount(String numberAccount, String holderAccount) {
+		this.numberAccount = numberAccount;
+		this.holderAccount = holderAccount;
+	}
+
+	public String getNumberAccount() {
+		return numberAccount;
+	}
+
+	public String getHolderAccount() {
+		return holderAccount;
+	}
+
+	public void setHolderAccount(String holderAccount) {
+		this.holderAccount = holderAccount;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void deposit (double balance) {
+		this.balance += balance;
+	}
+	
+	public void withdraw (double balance) {
+		this.balance -= (balance + 5);
+	}
+	
+	public String toString() {
+		return "Updated account data: \n"
+	    + "Number Account: " + getNumberAccount() + "\n" 
+	    + "Holder Account: " + getHolderAccount() + "\n"
+	    + "Balance: " + String.format("%.2f", getBalance());
+	}
+
+}
